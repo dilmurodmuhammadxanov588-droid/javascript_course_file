bd12d0c15a65ec25de9bc3f0d1d721e6941643b4
dilmurodmuhammadxanov588-droid <dilmurodmuhammadxanov588@gmail.com>
2025-10-08 15:43:50 +0500

    operators

---
+++ 3-lesson/js/index.js
@@ -0,0 +1,114 @@
+//   operatorlar
+//  let num1 =10;
+//  let str = "diyora";
+//  let a =num1 +str;
+//  console.log(a);
+//  console.log("salom"+ 12);
+
+//  let num1= 10;
+//  let num2= 5;
+//  let a= num1%num2;
+//  console.log(a);
+
+//  let num1 = 10;
+//  num1++;
+//  num1--;
+//  console.log(num1++);
+//  console.log(++num1);
+
+
+
+//let num1 =16;
+ //let num2 =10;
+ //console.log(num1+num2);
+
+
+
+// let num1 =30;
+// let num2 =20;
+//console.log(num1-num2);
+
+// // // // // // // // // // let num1 =15;
+// // // // // // // // // // let num2 =2;
+// // // // // // // // // // console.log(num1*num2);
+
+
+
+
+// // // // // // // // // let num1 =30;
+// // // // // // // // // let num2 =2;
+// // // // // // // // // console.log(num1/num2);
+
+
+
+// // // // // // // // //solishtirish operatorlari
+
+// // // // // // // // // let num1 = 10;
+// // // // // // // // // let num2 = 11;
+// // // // // // // // // console.log(num1<num2);
+
+
+
+// // // // // // // // let num1 = 10;
+// // // // // // // // let num2 = 10;
+// // // // // // // // console.log(num1<=num2);
+
+
+
+
+
+// // // // // // // let num1 = 10;
+// // // // // // // let num2 = 10;
+// // // // // // // console.log(num1==num2);
+
+
+
+// // // // // // let num1 = 10;
+// // // // // // let num2 = 10;
+// // // // // // console.log(num1!=num2);
+
+
+
+// // // // // // let num1 = 12;
+// // // // // // num1 +=10;
+// // // // // // console.log(num1);
+
+
+// // // // // let num1 = 12;
+// // // // // num1 -=10;
+// // // // // console.log(num1);
+
+
+
+// // // // let num1 = 12;
+// // // // num1 *=10;
+// // // // console.log(num1);
+
+
+// // // let num1 = 12;
+// // // num1 /=10;
+// // // console.log(num1);
+// // console.log(0 && true && true);
+// // console.log(1 || false || false);
+
+
+// console.log("a">"A");
+// console.log("a">"A");
+
+
+
+// let num1 = 80;
+// let num2 = 120;
+// console.log(num1*num2);
+
+
+let num1 = 60;
+let num2 =81;
+console.log(num1/num2);
+
+
+let number = 15;
+let number2 =13;
+console.log(number*number2);
+
+
