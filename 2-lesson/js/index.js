c565889806be69e345cfeeec40cc756e0c9905e5
dilmurodmuhammadxanov588-droid <dilmurodmuhammadxanov588@gmail.com>
2025-10-08 15:42:40 +0500

    data type

---
+++ 2-lesson/js/index.js
@@ -0,0 +1,81 @@
+// // Data type ....
+// // Primativ and non promativ (referens object)
+// // number
+// let nums1 = 13;
+// let nums2 = 12;
+// let nums3 =nums1+nums2;
+// console.log(nums3);
+
+
+
+
+// // string
+
+// let str = "diyora";
+// let str2= 'diyora ';
+// let str3 = `diyora ${str2}`;
+// console.log(str);
+// console.log(str2)
+// console.log(str3);
+// // bollean +> true and false
+// if (12 < 12) {
+//     console.log(true);
+// }else{
+//     console.log(false);
+    
+// }
+
+// undenfined
+let a;
+console.log(a);
+console.log(typeof a);
+
+
+
+// null
+let b =null;
+console.log(typeof b);
+
+// bigint 
+let son = 1111111111111111111n;
+console.log(typeof son, son);
+// nnon primiive 
+// object array function
+// let arry = ["Diyora,12,"]
+
+
+// arry 
+// let arry = ["diyora 12 true false null"]""
+// console.log(arry);
+
+
+
+
+// object
+
+
+// let obj ={
+// ism:"Diyora",
+// yoshi: 22,
+// };
+// console.log(obj.ism, obj.yoshi);
+
+// // function 
+// function harakat(){
+// console.log("hello world");
+// }
+// harakat();
+// let machine = ["mercedes,bmw,audi,nisan,"]
+// console.log(machine );
+let obj = {
+    rusumi:"iphone",
+    modeli:11,
+}
+console.log(obj.rusumi, obj.modeli);
+
+
+
+
+
+
+
